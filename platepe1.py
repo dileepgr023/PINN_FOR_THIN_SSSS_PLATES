ee35fc97b5989bf96e6931e41b794256e398c0fd
Dileep <dileepgr023@gmail.com>
2026-05-22 15:16:22 +0530

    PINN CODE FOR STRESS COUNTOUR

--- platepe1.py
+++ platepe1.py
@@ -13,7 +13,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Plate parameters
 q = 10000
 E= 200e9
-h = 0.001
+h = 0.01
 v = 0.3
 D = (E*h**3)/(12*(1-v**2))
 
@@ -26,6 +26,7 @@ class PINN(nn.Module):
             nn.Linear(2,64),
             nn.Tanh(),
             nn.Linear(64,64),
+            
             nn.Tanh(),
             nn.Linear(64,64),
             nn.Tanh(),
@@ -45,7 +46,7 @@ class PINN(nn.Module):
 
 model = PINN().to(device) 
 optimizer_adam = torch.optim.Adam(model.parameters(), lr=0.001) 
-# Optimizer     
+    
 optimizer_lbfgs = torch.optim.LBFGS(
     model.parameters(),
     lr=1.0,
@@ -60,6 +61,11 @@ N = 2000
 x = torch.rand(N,1, requires_grad=True, device=device)
 y = torch.rand(N,1, requires_grad=True, device=device)
 
+
+
+# epochs = 5000
+
+# for epoch  in range(epochs):
 
 epochs_adam = 2750
 
@@ -82,7 +88,7 @@ for epoch  in range(epochs_adam):
     energy_density = (w_xx**2+ w_yy**2+ 2*v*w_xx*w_yy + (2*(1-v)*w_xy**2))
     U = 0.5*D*torch.mean(energy_density)*1.0
     W = torch.mean(q*w)
-    ENERGY_LOSS = U-W
+    ENERGY_LOSS = (U-W)
   
     R_LOSS = ENERGY_LOSS
     
@@ -95,7 +101,7 @@ def closure():
     
     optimizer_lbfgs.zero_grad()
     
-    
+    # PDE residual
     w = model(x,y)
     
     w_x = torch.autograd.grad(w,x,torch.ones_like(w),create_graph=True)[0]
@@ -108,19 +114,23 @@ def closure():
     
     w_xy = torch.autograd.grad(w_x,y,torch.ones_like(w_x),create_graph=True)[0]
     
+    
+    
     energy_density = (w_xx**2+ w_yy**2+ 2*v*w_xx*w_yy + (2*(1-v)*w_xy**2))
     U = 0.5*D*torch.mean(energy_density)*1.0
     W = torch.mean(q*w)
-    ENERGY_LOSS = U-W
+    ENERGY_LOSS = (U-W)
   
     R_LOSS = ENERGY_LOSS
     
-   
+    
     total_loss = R_LOSS
 
     total_loss.backward()
     print("Loss:", total_loss.item())
-           
+            # if epoch % 500 == 0:
+            #     print(f"Epoch {epoch}, Total Loss: {total_loss.item():.6f}, PDE Loss: {resd_loss.item():.6f}, BC Loss: {BC_LOSS.item():.6f}")
+    return total_loss 
 
 optimizer_lbfgs.step(closure)
 
@@ -132,22 +142,57 @@ X,Y = torch.meshgrid(x_test,y_test)
 
 x_test = X.reshape(-1,1).to(device)
 y_test = Y.reshape(-1,1).to(device)
+x_test.requires_grad_(True)
+y_test.requires_grad_(True)
+w_pred = model(x_test,y_test)
 
-w_pred = model(x_test,y_test).detach().cpu().numpy()
-max_def = np.max(w_pred)
-print(max_def)
+w_x = torch.autograd.grad(w_pred,x_test,torch.ones_like(w_pred),create_graph=True)[0]
+w_y = torch.autograd.grad(w_pred,y_test,torch.ones_like(w_pred),create_graph=True)[0]
+w_xx = torch.autograd.grad(w_x,x_test,torch.ones_like(w_x),create_graph=True)[0]
+w_yy = torch.autograd.grad(w_y,y_test,torch.ones_like(w_y),  create_graph=True)[0]
+w_xy = torch.autograd.grad(w_x,y_test,torch.ones_like(w_x),create_graph=True)[0]
 
-# -----------------------------
-# Plot
-# -----------------------------
-plt.figure()
-plt.tricontourf(
-    x_test.cpu().numpy().flatten(),
-    y_test.cpu().numpy().flatten(),
-    w_pred.flatten(),
-    50
-)
-plt.colorbar()
-plt.title("Plate Deflection (PINN)")
-plt.show()
+z = h/2
+sigma_x = -(E*z/(1-v**2))*(w_xx + v*w_yy)
+sigma_y = -(E*z/(1-v**2))*(w_yy + v*w_xx)
+tau_xy = -(E*z/(1-v))*w_xy
 
+w_np = w_pred.cpu().detach().numpy()
+sigma_x_np = sigma_x.cpu().detach().numpy()
+sigma_y_np = sigma_y.cpu().detach().numpy()
+tau_xy_np = tau_xy.cpu().detach().numpy()
+
+print("Max deflection:", np.max(w_np))
+
+print("Max sigma_x:", np.max(sigma_x_np))
+
+print("Max sigma_y:", np.max(sigma_y_np))
+
+print("Max tau_xy:", np.max(tau_xy_np))
+
+
+
+# # -----------------------------
+# # Plot
+# # -----------------------------
+
+
+def plot_countour(x,title):
+    plt.figure()
+    plt.tricontourf(
+        x_test.detach().cpu().numpy().flatten(),
+        y_test.detach().cpu().numpy().flatten(),
+        x.flatten(),
+        50
+    )
+    plt.colorbar()
+    plt.title(title)
+    plt.show()
+   
+
+plot_countour(w_np,"Plate Deflection (PINN)")
+plot_countour(sigma_x_np,"Plate Stress_xx (PINN)")
+plot_countour(sigma_y_np,"Plate Stress_yy (PINN)")
+plot_countour(tau_xy_np,"Plate Shear Stress_xy (PINN)")
+
+    
